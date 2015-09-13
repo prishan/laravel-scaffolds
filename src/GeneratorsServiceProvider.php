3cdfa744c34a7fdd154c128e1b2115abc9ccab1c
Prishan Fernando <prishan.fernando@gmail.com>
2015-09-13 22:49:38 +1000

    fix folder structure

--- src/GeneratorsServiceProvider.php
+++ src/GeneratorsServiceProvider.php
@@ -36,7 +36,7 @@ class GeneratorsServiceProvider extends ServiceProvider {
 	private function registerScaffoldGenerator()
 	{
 		$this->app->singleton('command.larascaf.scaffold', function ($app) {
-			return $app['Prishan\Laravel-scaffolds\Commands\ScaffoldMakeCommand'];
+			return $app['Prishan\LaravelScaffolds\Commands\ScaffoldMakeCommand'];
 		});
 
 		$this->commands('command.larascaf.scaffold');
